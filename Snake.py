d8f93abc2a0e975c1ad9febd05b2da30a74ed966
Yummamuang <oak.ptkn@gmail.com>
2024-12-11 23:02:02 +0700

    New Project

---
+++ Snake.py
@@ -0,0 +1,130 @@
+from abc import abstractmethod
+
+
+class SnakeClass(object):
+
+    def __init__(self):
+        self.__name = ''
+        self.__position = []
+        self.__body = []
+        self.__enemy_body = []
+        self.__artifact_position = []
+        self.__fruit_position = []
+        self.__safe_zone_min_x = 0
+        self.__safe_zone_max_x = 0
+        self.__safe_zone_min_y = 0
+        self.__safe_zone_max_y = 0
+        self.__score = 0
+        self.__enemy_score = 0
+        self.__move_locked_status = False
+
+    # snake's name
+    def set_name(self, input_name: str):
+        self.__name = input_name
+
+    def get_name(self) -> str:
+        return self.__name
+
+    # snake's position
+    def set_position(self, pos: list):
+        self.__position = pos
+
+    def get_position(self) -> list:
+        return self.__position
+
+    # snake's body
+    def set_body(self, input_body: list):
+        self.__body = input_body
+
+    def insert_body(self, pos: list):
+        self.__body.insert(0, pos)
+
+    def get_body(self) -> list:
+        return self.__body
+
+    def body_pop(self):
+        self.__body.pop()
+
+    # intelligence information report
+    def set_safe_zone(self, input_boundary: list):
+        self.__safe_zone_min_x = input_boundary[0]
+        self.__safe_zone_max_x = input_boundary[1]
+        self.__safe_zone_min_y = input_boundary[2]
+        self.__safe_zone_max_y = input_boundary[3]
+
+    # enemy's body
+    def update_enemy_body(self, pos: list):
+        self.__enemy_body = pos
+
+    def get_enemy_body(self) -> list:
+        return self.__enemy_body
+
+    # artifact
+    def update_artifact_position(self, pos: list):
+        self.__artifact_position = pos
+
+    def get_artifact_position(self) -> list:
+        return self.__artifact_position
+
+    # fruit
+    def update_fruit_position(self, pos: list):
+        self.__fruit_position = pos
+
+    def get_fruit_position(self) -> list:
+        return self.__fruit_position
+
+    # score
+    def update_score(self, score: int):
+        self.__score = score
+
+    def get_score(self) -> int:
+        return self.__score
+
+    def update_enemy_score(self, score: int):
+        self.__enemy_score = score
+
+    def get_enemy_score(self) -> int:
+        return self.__enemy_score
+
+    # move lock
+    def move_lock(self):
+        # FOR SNAKE CLASS ONLY
+        self.__move_locked_status = True
+
+    def move_unlock_FOR_MAIN_ONLY(self):
+        # FOR MAIN ONLY
+        self.__move_locked_status = False
+
+    # snake move function
+    def up(self):
+        if self.__move_locked_status == 0:
+            self.__position[0] = self.__position[0]
+            self.__position[1] -= 10  # 10 unit per step
+            self.insert_body([self.__position[0], self.__position[1]])
+            self.move_lock()
+
+    def down(self):
+        if self.__move_locked_status == 0:
+            self.__position[0] = self.__position[0]
+            self.__position[1] += 10  # 10 unit per step
+            self.insert_body([self.__position[0], self.__position[1]])
+            self.move_lock()
+
+    def left(self):
+        if self.__move_locked_status == 0:
+            self.__position[0] -= 10  # 10 unit per step
+            self.__position[1] = self.__position[1]
+            self.insert_body([self.__position[0], self.__position[1]])
+            self.move_lock()
+
+    def right(self):
+        if self.__move_locked_status == 0:
+            self.__position[0] += 10  # 10 unit per step
+            self.__position[1] = self.__position[1]
+            self.insert_body([self.__position[0], self.__position[1]])
+            self.move_lock()
+
+    @abstractmethod
+    def snake_control(self):
+        # override this method
+        pass
---
+++ Snake2.py
@@ -0,0 +1,315 @@
+from Snake import SnakeClass
+import random
+
+
+class Snake2(SnakeClass):
+
+    def __init__(self):
+        super().__init__()
+        self.set_name('TEAM Khee Kai Deun 2')  # <--- define team's name here
+        self.visited_path = []
+
+    def get_visited_path(self) -> list:
+        return self.visited_path
+
+    def add_visited_path(self, step: list):
+        self.visited_path.insert(0, step)
+
+    def avoid(self, m):
+        # avoid danger zone
+
+        if self.get_position()[0] - 10 < self._SnakeClass__safe_zone_min_x:
+            m[0] = 'no'
+
+        if self.get_position()[0] + 10 > self._SnakeClass__safe_zone_max_x - 10:
+            m[1] = 'no'
+
+        if self.get_position()[1] - 10 < self._SnakeClass__safe_zone_min_y:
+            m[2] = 'no'
+
+        if self.get_position()[1] + 10 > self._SnakeClass__safe_zone_max_y - 10:
+            m[3] = 'no'
+
+        # avoid artifact
+        if self.get_position()[0] - 10 == self.get_artifact_position()[0] and self.get_position()[1] == \
+                self.get_artifact_position()[1]:
+            m[0] = 'no'
+
+        if self.get_position()[0] + 10 == self.get_artifact_position()[0] and self.get_position()[1] == \
+                self.get_artifact_position()[1]:
+            m[1] = 'no'
+
+        if self.get_position()[0] == self.get_artifact_position()[0] and self.get_position()[1] - 10 == \
+                self.get_artifact_position()[1]:
+            m[2] = 'no'
+
+        if self.get_position()[0] == self.get_artifact_position()[0] and self.get_position()[1] + 10 == \
+                self.get_artifact_position()[1]:
+            m[3] = 'no'
+
+        # avoid self touching
+        for block in self.get_body()[1:]:
+            for i in range(10, 11, 10):
+                if self.get_position()[0] - i == block[0] and self.get_position()[1] == block[1]:
+                    m[0] = 'no'
+
+                if self.get_position()[0] + i == block[0] and self.get_position()[1] == block[1]:
+                    m[1] = 'no'
+
+                if self.get_position()[0] == block[0] and self.get_position()[1] - i == block[1]:
+                    m[2] = 'no'
+
+                if self.get_position()[0] == block[0] and self.get_position()[1] + i == block[1]:
+                    m[3] = 'no'
+
+        for block in self.get_enemy_body():
+            if self.get_position()[0] - 10 == block[0] and self.get_position()[1] == block[1]:
+                m[0] = 'no'
+
+            if self.get_position()[0] + 10 == block[0] and self.get_position()[1] == block[1]:
+                m[1] = 'no'
+
+            if self.get_position()[0] == block[0] and self.get_position()[1] - 10 == block[1]:
+                m[2] = 'no'
+
+            if self.get_position()[0] == block[0] and self.get_position()[1] + 10 == block[1]:
+                m[3] = 'no'
+    # override control method here
+
+    def snake_control(self):
+        # movement: up, down, left, right
+
+        # self.left()
+
+        if self.get_position() != self.get_fruit_position():
+            # rand_case = random.randint(0, 1)
+            m = ['ok', 'ok', 'ok', 'ok']
+            special = False
+            self.avoid(m)
+
+
+# -------------------------------------------
+            if m.count('ok') == 1:
+                if m[0] == 'ok':
+                    self.left()
+                    self.add_visited_path(self.get_position())
+                    return 0
+                elif m[1] == 'ok':
+                    self.right()
+                    self.add_visited_path(self.get_position())
+                    return 0
+                elif m[2] == 'ok':
+                    self.up()
+                    self.add_visited_path(self.get_position())
+                    return 0
+                elif m[3] == 'ok':
+                    self.down()
+                    self.add_visited_path(self.get_position())
+                    return 0
+
+            if m.count('ok') < 4 and len(self.get_body()) > 20:
+                for block in self.get_body()[1:]:
+                    bo = ['ok', 'ok', 'ok', 'ok']
+                    for i in range(10, 100, 10):
+                        if self.get_position()[0] - i == block[0] and self.get_position()[1] == block[1]:
+                            bo[0] = 'no'
+
+                        if self.get_position()[0] + i == block[0] and self.get_position()[1] == block[1]:
+                            bo[1] = 'no'
+
+                        if self.get_position()[0] == block[0] and self.get_position()[1] - i == block[1]:
+                            bo[2] = 'no'
+
+                        if self.get_position()[0] == block[0] and self.get_position()[1] + i == block[1]:
+                            bo[3] = 'no'
+                    if m[0] == 'ok' and m[1] == 'ok':
+                        if bo[0] == 'no':
+                            self.right()
+                            self.add_visited_path(self.get_position())
+                            return 0
+                        elif bo[1] == 'no':
+                            self.left()
+                            self.add_visited_path(self.get_position())
+                            return 0
+                    elif m[2] == 'ok' and m[3] == 'ok':
+                        if bo[2] == 'no':
+                            self.down()
+                            self.add_visited_path(self.get_position())
+                            return 0
+                        elif bo[3] == 'no':
+                            self.up()
+                            self.add_visited_path(self.get_position())
+                            return 0
+                    elif m[1] == 'ok' and m[3] == 'ok':
+                        if bo[1] == 'ok':
+                            self.right()
+                            self.add_visited_path(self.get_position())
+                            return 0
+                        elif bo[3] == 'ok':
+                            self.down()
+                            self.add_visited_path(self.get_position())
+                            return 0
+                    elif m[0] == 'ok' and m[2] == 'ok':
+                        if bo[0] == 'ok':
+                            self.left()
+                            self.add_visited_path(self.get_position())
+                            return 0
+                        elif bo[2] == 'ok':
+                            self.up()
+                            self.add_visited_path(self.get_position())
+                            return 0
+                    elif m[0] == 'ok' and m[3] == 'ok':
+                        if bo[0] == 'ok':
+                            self.left()
+                            self.add_visited_path(self.get_position())
+                            return 0
+                        elif bo[3] == 'ok':
+                            self.up()
+                            self.add_visited_path(self.get_position())
+                            return 0
+                    else:
+                        ready_move = []
+
+                        if m[0] == 'ok':
+                            ready_move.append('ml')
+                        if m[1] == 'ok':
+                            ready_move.append('mr')
+                        if m[2] == 'ok':
+                            ready_move.append('mu')
+                        if m[3] == 'ok':
+                            ready_move.append('md')
+
+                        ready_move_len = len(ready_move)
+                        rand_idx = 0
+                        if ready_move_len > 1:
+                            rand_idx = random.randint(0, ready_move_len - 1)
+
+                        if ready_move_len > 0:
+                            if ready_move[rand_idx] == 'ml' and m[1] == 'ok':
+                                self.left()
+                                self.add_visited_path(self.get_position())
+                            elif ready_move[rand_idx] == 'mr' and m[1] == 'ok':
+                                self.right()
+                                self.add_visited_path(self.get_position())
+                            elif ready_move[rand_idx] == 'mu' and m[2] == 'ok':
+                                self.up()
+                                self.add_visited_path(self.get_position())
+                            elif ready_move[rand_idx] == 'md' and m[3] == 'ok':
+                                self.down()
+                                self.add_visited_path(self.get_position())
+                            return 0
+
+            for block in self.get_enemy_body():
+                if self.get_position()[0] - 10 == block[0] and self.get_position()[1] == block[1]:
+                    if m[2] == 'no':
+                        self.down()
+                        self.add_visited_path(self.get_position())
+                    else:
+                        self.up()
+                        self.add_visited_path(self.get_position())
+                    return 0
+
+                elif self.get_position()[0] + 10 == block[0] and self.get_position()[1] == block[1]:
+                    if m[2] == 'no':
+                        self.down()
+                        self.add_visited_path(self.get_position())
+                    else:
+                        self.up()
+                        self.add_visited_path(self.get_position())
+                    return 0
+
+                elif self.get_position()[0] == block[0] and self.get_position()[1] - 10 == block[1]:
+                    if m[0] == 'no':
+                        self.right()
+                        self.add_visited_path(self.get_position())
+                    else:
+                        self.left()
+                        self.add_visited_path(self.get_position())
+                    return 0
+
+                elif self.get_position()[0] == block[0] and self.get_position()[1] + 10 == block[1]:
+                    if m[0] == 'no':
+                        self.right()
+                        self.add_visited_path(self.get_position())
+                    else:
+                        self.left()
+                        self.add_visited_path(self.get_position())
+                    return 0
+
+
+# ---------------------------------------
+
+            if self.get_position()[0] > self.get_fruit_position()[0] and m[0] == 'ok':
+                self.left()
+                self.add_visited_path(self.get_position())
+            elif self.get_position()[0] < self.get_fruit_position()[0] and m[1] == 'ok':
+                self.right()
+                self.add_visited_path(self.get_position())
+            elif self.get_position()[1] < self.get_fruit_position()[1] and m[3] == 'ok':
+                self.down()
+                self.add_visited_path(self.get_position())
+            elif self.get_position()[1] > self.get_fruit_position()[1] and m[2] == 'ok':
+                self.up()
+                self.add_visited_path(self.get_position())
+            else:
+                ready_move = []
+
+                if m[0] == 'ok':
+                    ready_move.append('ml')
+                if m[1] == 'ok':
+                    ready_move.append('mr')
+                if m[2] == 'ok':
+                    ready_move.append('mu')
+                if m[3] == 'ok':
+                    ready_move.append('md')
+
+                ready_move_len = len(ready_move)
+                rand_idx = 0
+                if ready_move_len > 1:
+                    rand_idx = random.randint(0, ready_move_len - 1)
+
+                if ready_move_len > 0:
+                    if ready_move[rand_idx] == 'ml' and m[1] == 'ok':
+                        self.left()
+                        self.add_visited_path(self.get_position())
+                    elif ready_move[rand_idx] == 'mr' and m[1] == 'ok':
+                        self.right()
+                        self.add_visited_path(self.get_position())
+                    elif ready_move[rand_idx] == 'mu' and m[2] == 'ok':
+                        self.up()
+                        self.add_visited_path(self.get_position())
+                    elif ready_move[rand_idx] == 'md' and m[3] == 'ok':
+                        self.down()
+                        self.add_visited_path(self.get_position())
+                    return 0
+        else:
+            ready_move = []
+
+            if m[0] == 'ok':
+                ready_move.append('ml')
+            if m[1] == 'ok':
+                ready_move.append('mr')
+            if m[2] == 'ok':
+                ready_move.append('mu')
+            if m[3] == 'ok':
+                ready_move.append('md')
+
+            ready_move_len = len(ready_move)
+            rand_idx = 0
+            if ready_move_len > 1:
+                rand_idx = random.randint(0, ready_move_len - 1)
+
+            if ready_move_len > 0:
+                if ready_move[rand_idx] == 'ml' and m[1] == 'ok':
+                    self.left()
+                    self.add_visited_path(self.get_position())
+                elif ready_move[rand_idx] == 'mr' and m[1] == 'ok':
+                    self.right()
+                    self.add_visited_path(self.get_position())
+                elif ready_move[rand_idx] == 'mu' and m[2] == 'ok':
+                    self.up()
+                    self.add_visited_path(self.get_position())
+                elif ready_move[rand_idx] == 'md' and m[3] == 'ok':
+                    self.down()
+                    self.add_visited_path(self.get_position())
+                return 0
